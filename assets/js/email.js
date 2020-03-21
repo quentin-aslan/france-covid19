7892e69595b93788ef6ba9c49ceb49be38b545b8
Quentin Aslan <quentin.aslan@outlook.com>
2020-03-21 02:01:57 +0100

    Ajout d'un formulaire de newsletter

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,50 +1,49 @@
-$('#errorEmail').hide();
-$('#loadEmail').hide();
-$('#okEmail').hide();
-$('#loadDon').hide();
+$('.errorEmail').hide();
+$('.loadEmail').hide();
+$('.okEmail').hide();
+$('.loadDon').hide();
 $(() => {
-    $('#sendEmail').click((e) => {
-      e.preventDefault();
-        $('#loadEmail').show();
-        $('#sendEmail').hide();
-        
-        
+  $('.sendEmail').click((e) => {
+    e.preventDefault();
+    $('.loadEmail').show();
+    $('.sendEmail').hide();
 
-        
-        let email = $('#emailInput').val();
-        console.log(email);
-        
-        if(isEmail(email)) {
-          console.log('ok');
-            $.get(`http://admin.quentin-aslan.site/email/${email}`, function( data ) {
-              }).done(function() {
-                $('#okEmail').show();
-                $('#loadEmail').hide();
-                $('#errorEmail').hide();
-              })
-              .fail(function() {
-                $('#sendEmail').show();
-                $('#loadEmail').hide();
-                alert('Une erreur est survenu, veuillez appuyer une seconde fois sur le bouton "Envoyé');
-              });
 
-        } else {
-          console.log('ko');
-            $('#errorEmail').show();
-            $('#errorEmail').html('Vous devez rentrer une email valide.')
-            $('#sendEmail').show();
-            $('#loadEmail').hide();
-        }
 
+
+    let email = $('.emailInput').val();
+    console.log(email);
+
+    console.log('ok');
+    $.get(`http://admin.quentin-aslan.site/email/${email}`, function (data) {
+    }).done(function (res) {
+      if (res == 'ok') {
+        $('.okEmail').show();
+        $('.loadEmail').hide();
+        $('.errorEmail').hide();
+      } else {
+        $('.errorEmail').show();
+        $('.errorEmail').html('Vous devez rentrer une adresse email valide.')
+        $('.sendEmail').show();
+        $('.loadEmail').hide();
+      }
+    })
+    .fail(function () {
+      $('.sendEmail').show();
+      $('.loadEmail').hide();
+      alert('Une erreur est survenu, veuillez appuyer une seconde fois sur le bouton "Envoyé');
     });
 
-    $('#donPaypal').click((e) => {
-      $('#loadDon').show();
-    });
+
+
+});
+
+$('.donPaypal').click((e) => {
+  $('.loadDon').show();
+});
 
 });
 
 function isEmail(email) {
-    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    return regex.test(email);
-  }
+  return true;
+}
